935b1b72150d97fc03275ca6639b0934eb1d84be
kenxsantos <kenxsantos@gmail.com>
2025-04-07 17:29:20 +0800

    feat: refactor favorite page to improve item filtering and display logic

--- lib/shopping/screens/favorite_page.dart
+++ lib/shopping/screens/favorite_page.dart
@@ -15,65 +15,43 @@ class _FavoritePageState extends State<FavoritePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 16.0),
       color: Colors.white,
-      width: double.infinity,
-      height: double.infinity,
+      padding: EdgeInsets.all(16),
       child: BlocBuilder<ShoppingBloc, ShoppingState>(
         builder: (context, state) {
-          if (state is ShoppingListLoaded) {
-            final items = state.items.where((item) => item.isFavorite).toList();
-            if (items.isEmpty) {
-              return Center(child: Text('No items in the favorites.'));
-            } else {
-              return ListView.builder(
-                itemCount: items.length,
-                itemBuilder: (context, index) {
-                  final item = items[index];
-                  print(item.name);
-                  if (item.isFavorite) {
-                    return Card(
-                      child: ItemTile(
-                        id: item.id,
-                        name: item.name,
-                        tag: item.tag,
-                        isFavorite: item.isFavorite,
-                      ),
-                    );
-                  }
-                  return SizedBox.shrink();
-                },
-              );
-            }
-          } else if (state is ShoppingListFiltered) {
-            final items =
-                state.filteredItems.where((item) => item.isFavorite).toList();
-            if (items.isEmpty) {
-              return Center(child: Text('No items in the favorites.'));
-            } else {
-              return ListView.builder(
-                itemCount: items.length,
-                itemBuilder: (context, index) {
-                  final item = items[index];
-                  print(item.name);
-                  if (item.isFavorite) {
-                    return Card(
-                      child: ItemTile(
-                        id: item.id,
-                        name: item.name,
-                        tag: item.tag,
-                        isFavorite: item.isFavorite,
-                      ),
-                    );
-                  }
-                  return SizedBox.shrink();
-                },
-              );
-            }
-          }
-          return Center(child: Text('No items in the favorites.'));
+          final favoriteItems = _getFavoriteItems(state);
+          return favoriteItems.isEmpty
+              ? const Center(child: Text('No items in the favorites.'))
+              : _buildItemList(favoriteItems);
         },
       ),
+    );
+  }
+
+  List _getFavoriteItems(ShoppingState state) {
+    if (state is ShoppingListLoaded) {
+      return state.items.where((item) => item.isFavorite).toList();
+    } else if (state is ShoppingListFiltered) {
+      return state.filteredItems.where((item) => item.isFavorite).toList();
+    }
+    return [];
+  }
+
+  Widget _buildItemList(List favoriteItems) {
+    return ListView.builder(
+      itemCount: favoriteItems.length,
+      itemBuilder: (context, index) {
+        final item = favoriteItems[index];
+        return Card(
+          color: Colors.grey[800],
+          child: ItemTile(
+            id: item.id,
+            name: item.name,
+            tag: item.tag,
+            isFavorite: item.isFavorite,
+          ),
+        );
+      },
     );
   }
 }
